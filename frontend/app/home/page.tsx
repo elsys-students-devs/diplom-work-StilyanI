eed4c939383e7a00425fcdacb578aa0dfa08dfe6
StilyanI <103041263+StilyanI@users.noreply.github.com>
2025-12-05 11:16:08 +0200

    Created home page base

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,11 +1,15 @@
-import { AppBar, Box, Container, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { AccountCircle } from "@mui/icons-material";
+import Link from "next/link";
 
 export default function HomePage() {
     return(
         <Box>
-            <AppBar>
+            <AppBar
+                sx={{backgroundColor: "gray"}}
+                position="sticky"
+            >
                 <Toolbar>
                     <IconButton>
                         <MenuIcon/>
@@ -13,11 +17,28 @@ export default function HomePage() {
 
                     <Box sx={{flexGrow: 1}}/>
 
+                    <Link href={"/movies"}>
+                        <Typography sx={{color: "black"}}>Movies</Typography>
+                    </Link>
+
+                    <Box sx={{mx: 2}}/>
+
+                    <Link href={"/shows"}>
+                        <Typography sx={{color: "black"}}>Shows</Typography>
+                    </Link>
+
+                    <Box sx={{flexGrow: 1}}/>
+
                     <IconButton>
                         <AccountCircle/>
                     </IconButton>
+                    
                 </Toolbar>
             </AppBar>
+
+            <Typography variant="h4">Continue Watching</Typography>
+            <Typography variant="h4">Movies</Typography>
+            <Typography variant="h4">Shows</Typography>
         </Box>
     )
 }
