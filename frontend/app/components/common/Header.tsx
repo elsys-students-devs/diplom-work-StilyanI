902129ed4842c435fe1161f8591cd40080aa0a15
StilyanI <stilyan.i.ivanov@gmail.com>
2026-02-02 21:41:43 +0200

    Added player to frontend

--- frontend/app/components/common/Header.tsx
+++ frontend/app/components/common/Header.tsx
@@ -30,83 +30,86 @@ export default function Header(){
     }
 
     return (
-        <div>
-            <AppBar
-                sx={{backgroundColor: "gray"}}
-                position="sticky"
-            >
-                <Toolbar
-                    sx={{display: "flex", justifyContent: "space-between"}}
+        (pathname === "/player") ?
+            <div></div> : <div>
+                <AppBar
+                    sx={{backgroundColor: "gray"}}
+                    position="sticky"
                 >
-                    <Box>
-                        <IconButton onClick={toggleDrawer}>
-                            <MenuIcon/>
+                    <Toolbar
+                        sx={{display: "flex", justifyContent: "space-between"}}
+                    >
+                        <Box>
+                            <IconButton onClick={toggleDrawer}>
+                                <MenuIcon/>
+                            </IconButton>
+
+                            <IconButton component={Link} href={"/home"}
+                                        sx={{visibility: pathname === "/home" ? "hidden" : "visible"}}>
+                                <HomeIcon/>
+                            </IconButton>
+
+                        </Box>
+
+                        <Box sx={{display: "flex"}}>
+                            {pathname === "/movies" ?
+                                <Typography className="unselectable-header-button">Movies</Typography>
+                                :
+                                <Typography component={Link} href={"/movies"} className="section-button">Movies</Typography>
+                            }
+
+                            <Box sx={{mx: 2}}/>
+
+                            {pathname === "/shows" ?
+                                <Typography className="unselectable-header-button">Shows</Typography>
+                                :
+                                <Typography component={Link} href={"/shows"} className="section-button">Shows</Typography>
+                            }
+
+                        </Box>
+
+                        <IconButton>
+                            <AccountCircleIcon/>
                         </IconButton>
 
-                        <IconButton component={Link} href={"/home"} sx={{visibility: pathname === "/home" ? "hidden" : "visible"}}>
-                            <HomeIcon/>
-                        </IconButton>
+                    </Toolbar>
+                </AppBar>
 
-                    </Box>
-
-                    <Box sx={{display: "flex"}}>
-                        {pathname === "/movies" ?
-                            <Typography className="unselectable-header-button">Movies</Typography>
-                             :
-                            <Typography component={Link} href={"/movies"} className="section-button">Movies</Typography>
-                        }
-
-                        <Box sx={{mx: 2}}/>
-
-                        {pathname === "/shows" ?
-                            <Typography className="unselectable-header-button">Shows</Typography>
-                            :
-                            <Typography component={Link} href={"/shows"} className="section-button">Shows</Typography>
-                        }
-
-                    </Box>
-
-                    <IconButton>
-                        <AccountCircleIcon/>
-                    </IconButton>
-
-                </Toolbar>
-            </AppBar>
-
-            <Drawer open={drawerOpen} onClose={toggleDrawer} slotProps={{paper: {sx: {backgroundColor:"gray"}} }} onClick={toggleDrawer}>
-                <Box sx={{width: 250}}>
-                    <List>
-                        {['Home', 'Movies', 'Shows'].map((text) => (
-                            <ListItem key={text}>
-                                <ListItemButton component={Link} href={"/" + text.toLowerCase()}>
-                                    <ListItemIcon>
-                                        {
+                <Drawer open={drawerOpen} onClose={toggleDrawer} slotProps={{paper: {sx: {backgroundColor: "gray"}}}}
+                        onClick={toggleDrawer}>
+                    <Box sx={{width: 250}}>
+                        <List>
+                            {['Home', 'Movies', 'Shows'].map((text) => (
+                                <ListItem key={text}>
+                                    <ListItemButton component={Link} href={"/" + text.toLowerCase()}>
+                                        <ListItemIcon>
                                             {
-                                                'Home': <HomeIcon/>,
-                                                'Movies': <LocalMoviesIcon/>,
-                                                'Shows': <TvIcon/>
-                                            }[text]
-                                        }
+                                                {
+                                                    'Home': <HomeIcon/>,
+                                                    'Movies': <LocalMoviesIcon/>,
+                                                    'Shows': <TvIcon/>
+                                                }[text]
+                                            }
+                                        </ListItemIcon>
+                                        <ListItemText primary={text}/>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+
+                            <Divider/>
+
+                            <ListItem key={'dashboard'}>
+                                <ListItemButton component={Link} href={"/dashboard"}>
+                                    <ListItemIcon>
+                                        <DashboardIcon/>
                                     </ListItemIcon>
-                                    <ListItemText primary={text}/>
+                                    <ListItemText primary={'Dashboard'}/>
                                 </ListItemButton>
                             </ListItem>
-                        ))}
 
-                        <Divider/>
-
-                        <ListItem key={'dashboard'}>
-                            <ListItemButton component={Link} href={"/dashboard"}>
-                                <ListItemIcon>
-                                    <DashboardIcon/>
-                                </ListItemIcon>
-                                <ListItemText primary={'Dashboard'}/>
-                            </ListItemButton>
-                        </ListItem>
-
-                    </List>
-                </Box>
-            </Drawer>
-        </div>
+                        </List>
+                    </Box>
+                </Drawer>
+            </div>
     )
 }
